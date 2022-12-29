509a9394015e259f254a132c51a724d610723f9e
Jacki Z <jzamitalo@icloud.com>
2022-12-29 17:47:05 -0500

    Implement a very basic version of program, with only black and white and variable pad sizes

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-const pad = document.createElement('div');
+// Variables
+const pad = document.querySelector('.container');
+let numDivsPerSide = 16;
+let totalDivs = numDivsPerSide * numDivsPerSide;
+
+const userChoiceTrigger = document.createElement('button');
+userChoiceTrigger.textContent = "Change pixel size";
+document.body.appendChild(userChoiceTrigger);
+userChoiceTrigger.addEventListener('click', () => {
+    createNewGrid();
+});
+
+// Styling
 const padStyle = {
     backgroundColor: 'aliceblue',
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
     height: '600px',
     width: '600px',
     position: 'absolute',
@@ -10,17 +25,54 @@ const padStyle = {
     bottom: 0,
     margin: 'auto'
 }
-const numDivs = 16;
-const divWidth = 600 / numDivs;
-const divHeight = divWidth;
-
-// Array.from(numDivs).forEach((div) => {
-//     document.createElement('div');
-//     div.style.height = divHeight;
-//     div.style.width = divWidth;
-//     div.style.backgroundColor = "yellow";
-//     document.body.appendChild(div);
-// });
-
 Object.assign(pad.style, padStyle);
-document.body.appendChild(pad);
+
+const boxStyle = {
+    width: 600 / numDivsPerSide + 'px',
+    height: 600 / numDivsPerSide + 'px',
+}
+
+// Functions
+function createFirstGrid(totalDivs) {
+    for (let i = 0; i < totalDivs; i++)
+    {
+        let box = document.createElement('div');
+        box.setAttribute('class', 'pixel');
+        box.addEventListener('mouseover', function(e) {
+            box.style.backgroundColor = 'black';
+        });
+        Object.assign(box.style, boxStyle);
+        pad.appendChild(box);
+    }
+}
+
+function getUserInput() {
+    numDivsPerSide = prompt("How many pixels would you like per side in the pad?", "16")
+    if (numDivsPerSide > 100 || numDivsPerSide < 0)
+    {
+        alert("You must enter a number from 1 to 100.");
+        return;
+    }
+    return numDivsPerSide;
+}
+
+function createNewGrid() {
+    const deletedEls = document.querySelectorAll('.pixel');
+    deletedEls.forEach((block) => {
+        block.remove();
+    })
+    numDivsPerSide = getUserInput();
+    totalDivs = numDivsPerSide * numDivsPerSide;
+    for (i = 0; i < totalDivs; i++)
+    {
+        let newBox = document.createElement('div');
+        newBox.setAttribute('class', 'pixel');
+        newBox.style.width = 600 / numDivsPerSide + 'px';
+        newBox.style.height = 600 / numDivsPerSide + 'px';
+        pad.appendChild(newBox);
+        newBox.addEventListener('mouseover', () => {
+            newBox.style.backgroundColor = 'black';
+        });
+    }
+}
+createFirstGrid(256);
