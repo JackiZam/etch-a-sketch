8800223c1e4a18ea8ce70361e1ed67cd2d11a14a
Jacki Z <jzamitalo@icloud.com>
2023-02-06 14:34:26 -0500

    Add different capabilites, such as reseting the pad and drawing click

--- index.js
+++ index.js
@@ -2,13 +2,69 @@
 const pad = document.querySelector('.container');
 let numDivsPerSide = 16;
 let totalDivs = numDivsPerSide * numDivsPerSide;
+let defaultBackgroundColor = 'aliceblue';
+let rainbowModeMarker = 0;
 
-const userChoiceTrigger = document.createElement('button');
-userChoiceTrigger.textContent = "Change pixel size";
-document.body.appendChild(userChoiceTrigger);
-userChoiceTrigger.addEventListener('click', () => {
-    createNewGrid();
+// Change Pixels 
+const changePixButton = document.createElement('button');
+changePixButton.textContent = "Change pixel size";
+document.body.appendChild(changePixButton);
+changePixButton.addEventListener('click', () => {
+    numDivsPerSide = getUserInput();
+    totalDivs = numDivsPerSide * numDivsPerSide;
+    createNewGrid(totalDivs);
 });
+
+// Reset Pixels
+const resetPadButton = document.createElement('button');
+resetPadButton.textContent = "Reset Pad";
+document.body.appendChild(resetPadButton);
+resetPadButton.addEventListener('click', () => {
+    resetGrid();
+})
+
+// Reset Size & Pixels
+const resetSizeButton = document.createElement('button');
+resetSizeButton.textContent = "Reset Pad & Pixel Size";
+document.body.appendChild(resetSizeButton);
+resetSizeButton.addEventListener('click', () => {
+    resetGrid();
+    numDivsPerSide = 16;
+    totalDivs = numDivsPerSide * numDivsPerSide;
+    createNewGrid(totalDivs);
+})
+
+// Draw on Hover
+const hoverDraw = document.createElement('button');
+hoverDraw.textContent = "Draw on hover";
+hoverDraw.addEventListener('click', () => {
+    totalDivs = numDivsPerSide  * numDivsPerSide;
+    createNewGrid(totalDivs, "hover");
+})
+
+// Draw on click
+const clickDraw = document.createElement('button');
+clickDraw.textContent = "Draw on click";
+clickDraw.addEventListener('click', () => {
+    createNewGrid(totalDivs, "click");
+})
+document.body.appendChild(hoverDraw);
+document.body.appendChild(clickDraw);
+
+// Rainbow mode button
+const rainbowMode = document.createElement('button');
+rainbowMode.textContent = "Rainbow Mode";
+rainbowMode.addEventListener('click', () => {
+    rainbowModeMarker = 1;
+    console.log(rainbowModeMarker);
+    createNewGrid(totalDivs), rainbowModeMarker;
+})
+document.body.appendChild(rainbowMode);
+
+const randomRed = Math.floor(Math.random() * 256);
+const randomBlue = Math.floor(Math.random() * 256);
+const randomGreen = Math.floor(Math.random() * 256);
+const randColor = `rgb(${randomRed}, ${randomBlue}, ${randomGreen})`;
 
 // Styling
 const padStyle = {
@@ -37,18 +93,107 @@ function createFirstGrid(totalDivs) {
     for (let i = 0; i < totalDivs; i++)
     {
         let box = document.createElement('div');
-        box.setAttribute('class', 'pixel');
-        box.addEventListener('mouseover', function(e) {
-            box.style.backgroundColor = 'black';
-        });
+        box.setAttribute('class', 'boxNotFilled');
+        box.setAttribute('id', 'pixel');
         Object.assign(box.style, boxStyle);
         pad.appendChild(box);
+
+        box.addEventListener('click', () => {
+            if (box.classList.contains('boxNotFilled')) {
+                box.style.backgroundColor = 'black';
+                box.classList.toggle('boxNotFilled');
+            }
+            else {
+                box.style.backgroundColor = defaultBackgroundColor;
+                box.classList.toggle('boxNotFilled');
+            }
+        });
+    }
+}
+
+
+/* Perhaps build another function like this, but one for hover mode, and the other for click mode, and they depend on rainbowModeMarker*/
+function createNewGrid(totalDivs, penType, rainbowModeMarker) {
+    const allBoxes = document.querySelectorAll('#pixel');
+    allBoxes.forEach((block) => {
+        block.remove();
+    })
+    for (i = 0; i < totalDivs; i++)
+    {
+        let randomRed = Math.floor(Math.random() * 256);
+        let randomBlue = Math.floor(Math.random() * 256);
+        let randomGreen = Math.floor(Math.random() * 256);
+        let randColor = `rgb(${randomRed}, ${randomBlue}, ${randomGreen})`;
+
+        let newBox = document.createElement('div');
+        newBox.setAttribute('class', 'boxNotFilled');
+        newBox.setAttribute('id', 'pixel');
+        newBox.style.width = 600 / numDivsPerSide + 'px';
+        newBox.style.height = 600 / numDivsPerSide + 'px';
+        pad.appendChild(newBox);
+        if (penType == "hover") {
+            if (rainbowModeMarker != 1) {
+                newBox.addEventListener('mouseover', () => {
+                    newBox.style.backgroundColor = 'black';
+                })
+                newBox.addEventListener('click', ()=> {
+                    if (rainbowModeMarker == 0) {
+                        if (newBox.classList.contains('boxNotFilled')) {
+                            newBox.style.backgroundColor = 'black';
+                            newBox.classList.toggle('boxNotFilled');
+                        }
+                        else {
+                            newBox.style.backgroundColor = defaultBackgroundColor;
+                            newBox.classList.toggle('boxNotFilled');
+                        }
+                    }
+                    else {
+                        if (newBox.classList.contains('boxNotFilled')) {
+                            newBox.style.backgroundColor = randColor;
+                            newBox.classList.toggle('boxNotFilled');
+                        }
+                        else {
+                            newBox.style.backgroundColor = defaultBackgroundColor;
+                            newBox.classList.toggle('boxNotFilled');
+                        }
+                    }
+            })}
+            else {
+                newBox.addEventListener('mouseover', () => {
+                    newBox.style.backgroundColor = randColor;
+                })
+            }
+        }
+        else {
+            newBox.addEventListener('click', () => {
+                if (rainbowModeMarker != 1) {
+                    if (newBox.classList.contains('boxNotFilled')) {
+                        newBox.style.backgroundColor = 'black';
+                        newBox.classList.toggle('boxNotFilled');
+                    }
+                    else {
+                        newBox.style.backgroundColor = defaultBackgroundColor;
+                        newBox.classList.toggle('boxNotFilled');
+                    }
+                }
+                else {
+                    if (newBox.classList.contains('boxNotFilled')) {
+                        newBox.style.backgroundColor = randColor;
+                        newBox.classList.toggle('boxNotFilled');
+                    }
+                    else {
+                        newBox.style.backgroundColor = defaultBackgroundColor;
+                        newBox.classList.toggle('boxNotFilled');
+                    }
+                }
+            });
+        }
     }
 }
 
 function getUserInput() {
     numDivsPerSide = prompt("How many pixels would you like per side in the pad?", "16")
-    if (numDivsPerSide > 100 || numDivsPerSide < 0)
+    if (numDivsPerSide > 100 || numDivsPerSide <= 0)
     {
         alert("You must enter a number from 1 to 100.");
         return;
@@ -56,23 +201,11 @@ function getUserInput() {
     return numDivsPerSide;
 }
 
-function createNewGrid() {
-    const deletedEls = document.querySelectorAll('.pixel');
-    deletedEls.forEach((block) => {
-        block.remove();
-    })
-    numDivsPerSide = getUserInput();
-    totalDivs = numDivsPerSide * numDivsPerSide;
-    for (i = 0; i < totalDivs; i++)
-    {
-        let newBox = document.createElement('div');
-        newBox.setAttribute('class', 'pixel');
-        newBox.style.width = 600 / numDivsPerSide + 'px';
-        newBox.style.height = 600 / numDivsPerSide + 'px';
-        pad.appendChild(newBox);
-        newBox.addEventListener('mouseover', () => {
-            newBox.style.backgroundColor = 'black';
-        });
+function resetGrid() {
+    let pixels = document.querySelectorAll('#pixel');
+    for (i = 0; i < pixels.length; i++) {
+        pixels[i].style.backgroundColor = defaultBackgroundColor;
     }
 }
+
 createFirstGrid(256);
